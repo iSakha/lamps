00c3289d2b42c5bd9d32cf835fc723b36a44c59d
Sakha <sakha60@gmail.com>
2019-11-14 21:54:02 +0300

    inputs added

--- newLamps/script.js
+++ newLamps/script.js
@@ -1,12 +1,12 @@
 function changeTab(_i) {
-  let array_tbl_id  = ['lampStoreT','lampIN_T','lampOUT_T','','lampStoreR','lampIN_R','lampOUT_R',''];
+  let array_tbl_id  = ['lampStoreT','lampIN_T','lampOUT_T','lampsINPUT_T','lampStoreR','lampIN_R','lampOUT_R','lampsINPUT_R'];
     let tab = [];
     let table = [];
     let tbl_id = array_tbl_id[_i];
     
     tab = document.querySelectorAll('.tab');
     table = document.getElementById(tbl_id);
-    console.log(table);
+
     for (let i = 0; i < tab.length; i++) {
       tab[i].style.display = 'none';
     if (switchMode[0].checked) {
@@ -28,6 +28,46 @@ let switchMode = document.getElementsByName('test/real');
             if (switchMode[0].checked) {changeTab(i);}
           else{changeTab(i + 4);}
         }
+      }
+
+      function createInputs() {
+
+        let myInput = document.createElement('input');
+
+        let parentMyInput = document.getElementById('inpt1');
+        parentMyInput.appendChild(myInput);
+        for (var i = 1; i < 5; i++) {
+          myInput = document.createElement('input');
+          parentMyInput = document.getElementById('inpt1');
+          parentMyInput.appendChild(myInput);
+          
+        }
+    
+    
+          parentMyInput = document.getElementById('inpt2');
+          parentMyInput.appendChild(myInput);
+          for (var i = 0; i < 3; i++) {
+            myInput = document.createElement('input');
+            parentMyInput = document.getElementById('inpt2');
+            parentMyInput.appendChild(myInput);
+          }
+          let myInputs = document.getElementsByTagName('input');
+            myInputs[2].placeholder = 'Lamp';
+            myInputs[2].id = 'lamp';
+            myInputs[3].placeholder = 'Fixture';
+            myInputs[3].id = 'fixture';
+            myInputs[4].placeholder = 'Location';
+            myInputs[4].id = 'locat';
+            myInputs[5].placeholder = 'Q-ty';
+            myInputs[5].id = 'qty';
+            myInputs[6].placeholder = 'Personnel';
+            myInputs[6].id = 'pers';
+            myInputs[7].placeholder = 'Notes1';
+            myInputs[7].id = 'notes1';
+            myInputs[8].placeholder = 'Notes2';
+            myInputs[8].id = 'notes2';
+            myInputs[9].placeholder = 'Reserve';
+            myInputs[9].id = 'reserve';
       }
 
 //===============   Store test table 1  ==================================
@@ -172,7 +212,52 @@ function remove_Table_3() {
 
 function createTable_4() {
 
+  let table = document.createElement('table');
+
+table.id = 'lampsINPUT_T';
+
+let parentTable = document.getElementById('tbl4');
+let myRow = document.createElement('tr');
+//console.log(myRow);
+let myCell = document.createElement('th');
+myCell.innerHTML = 'id <br>Лампы';
+myRow.appendChild(myCell);
+myCell = document.createElement('th');
+myCell.innerHTML = 'Номер';
+myRow.appendChild(myCell);
+myCell = document.createElement('th');
+myCell.innerHTML = 'Наименование';
+myRow.appendChild(myCell);
+myCell = document.createElement('th');
+myCell.innerHTML = 'PartNumber';
+myRow.appendChild(myCell);
+myCell = document.createElement('th');
+myCell.innerHTML = 'Прибор';
+myRow.appendChild(myCell);
+myCell = document.createElement('th');
+myCell.innerHTML = 'Время <br>службы';
+myRow.appendChild(myCell);
+myCell = document.createElement('th');
+myCell.innerHTML = 'Мощность';
+myRow.appendChild(myCell);
+myCell = document.createElement('th');
+myCell.innerHTML = 'Количество<br>на складе ';
+myRow.appendChild(myCell);
+myCell = document.createElement('th');
+myCell.innerHTML = 'Где лежит ';
+myRow.appendChild(myCell);
+
+
+table.appendChild(myRow);
+parentTable.appendChild(table);
+
 }
+
+function remove_Table_4() {
+  table = document.getElementById('lampsINPUT_T');
+  table.remove();
+}
+
 
 //===============   Store real table 5  ==================================
 
@@ -315,8 +400,51 @@ function remove_Table_7() {
 //===============   INPUT real table 8  ==================================
 
 function createTable_8() {
+  let table = document.createElement('table');
 
+table.id = 'lampsINPUT_R';
+
+let parentTable = document.getElementById('tbl8');
+let myRow = document.createElement('tr');
+//console.log(myRow);
+let myCell = document.createElement('th');
+myCell.innerHTML = 'id <br>Лампы';
+myRow.appendChild(myCell);
+myCell = document.createElement('th');
+myCell.innerHTML = 'Номер';
+myRow.appendChild(myCell);
+myCell = document.createElement('th');
+myCell.innerHTML = 'Наименование';
+myRow.appendChild(myCell);
+myCell = document.createElement('th');
+myCell.innerHTML = 'PartNumber';
+myRow.appendChild(myCell);
+myCell = document.createElement('th');
+myCell.innerHTML = 'Прибор';
+myRow.appendChild(myCell);
+myCell = document.createElement('th');
+myCell.innerHTML = 'Время <br>службы';
+myRow.appendChild(myCell);
+myCell = document.createElement('th');
+myCell.innerHTML = 'Мощность';
+myRow.appendChild(myCell);
+myCell = document.createElement('th');
+myCell.innerHTML = 'Количество<br>на складе ';
+myRow.appendChild(myCell);
+myCell = document.createElement('th');
+myCell.innerHTML = 'Где лежит ';
+myRow.appendChild(myCell);
+
+
+table.appendChild(myRow);
+parentTable.appendChild(table);
 }
+
+function remove_Table_8() {
+  table = document.getElementById('lampsINPUT_R');
+  table.remove();
+}
+
 
 function errData(err) {
   console.log('Error!');
@@ -477,59 +605,54 @@ function gotData_3(data) {
 
 //===============   Got data 4  ==================================
 
-// function gotData_1(data) {
+function gotData_4(data) {
 
-//   remove_Table_1();
-//   createTable_1();
+  remove_Table_4();
+  createTable_4();
 
-//   table = document.getElementById('lampStoreT');
+  table = document.getElementById('lampsINPUT_T');
 
-//   let array = new Array();
+  let array = new Array();
 
-//   var lamps = data.val();
-//   var keys = Object.keys(lamps);
+  var lamps = data.val();
+  var keys = Object.keys(lamps);
 
-//   for (var i = 0; i < keys.length; i++) {
-//     var k = keys[i];
+  for (var i = 0; i < keys.length; i++) {
+    var k = keys[i];
 
-//     array[0] = k;
-//     array[1] = lamps[k].lampID;
-//     array[2] = lamps[k].lampName;
-//     array[3] = lamps[k].partNumber;
-//     array[4] = lamps[k].fixture;
-//     array[5] = lamps[k].lifeTime;
-//     array[6] = lamps[k].power;
-//     array[7] = lamps[k].qty;
-//     array[8] = lamps[k].location;
+    array[0] = k;
+    array[1] = lamps[k].lampID;
+    array[2] = lamps[k].lampName;
+    array[3] = lamps[k].partNumber;
+    array[4] = lamps[k].fixture;
+    array[5] = lamps[k].lifeTime;
+    array[6] = lamps[k].power;
+    array[7] = lamps[k].qty;
+    array[8] = lamps[k].location;
 
-//     // create a new row
-//     var newRow = table.insertRow(i + 1);
+    // create a new row
+    var newRow = table.insertRow(i + 1);
 
-//     for (var j = 0; j < 9; j++) {
+    for (var j = 0; j < 9; j++) {
 
-//       // create a new cell
-//       var cell = newRow.insertCell(j);
+      // create a new cell
+      var cell = newRow.insertCell(j);
 
-//       // add value to the cell
-//       cell.innerHTML = array[j];
-//     }
-//   }
-//   // Hide first column of the table (with database keys)
-//   for (var i = 0; i <= keys.length; i++) {
-//     table.rows[i].cells[0].style.display = 'none';
-//   }
-//   for (var i = 1; i <= keys.length; i++) {
-//     table.rows[i].cells[2].id = 'colName';
-//     table.rows[i].cells[4].id = 'colFxt';
-//     table.rows[i].cells[7].id = 'colQty';
-//   }
-//   let testMode = document.getElementsByName('test/real');
-//   if (testMode[0].checked) {
-//     table.classList.add('borderColorGrey');
-//   } else {
-//     table.classList.add('borderColorRed');
-//   }
-// }
+      // add value to the cell
+      cell.innerHTML = array[j];
+    }
+  }
+  // Hide first column of the table (with database keys)
+  for (var i = 0; i <= keys.length; i++) {
+    table.rows[i].cells[0].style.display = 'none';
+  }
+  for (var i = 1; i <= keys.length; i++) {
+    table.rows[i].cells[2].id = 'colName';
+    table.rows[i].cells[4].id = 'colFxt';
+    table.rows[i].cells[7].id = 'colQty';
+  }
+
+}
 
 //------------------------------------------------------------------
 
@@ -685,13 +808,70 @@ function gotData_7(data) {
 
 //------------------------------------------------------------------
 
+//===============   Got data 8  ==================================
+
+function gotData_8(data) {
+
+  remove_Table_8();
+  createTable_8();
+
+  table = document.getElementById('lampsINPUT_R');
+
+  let array = new Array();
+
+  var lamps = data.val();
+  var keys = Object.keys(lamps);
+
+  for (var i = 0; i < keys.length; i++) {
+    var k = keys[i];
+
+    array[0] = k;
+    array[1] = lamps[k].lampID;
+    array[2] = lamps[k].lampName;
+    array[3] = lamps[k].partNumber;
+    array[4] = lamps[k].fixture;
+    array[5] = lamps[k].lifeTime;
+    array[6] = lamps[k].power;
+    array[7] = lamps[k].qty;
+    array[8] = lamps[k].location;
+
+    // create a new row
+    var newRow = table.insertRow(i + 1);
+
+    for (var j = 0; j < 9; j++) {
+
+      // create a new cell
+      var cell = newRow.insertCell(j);
+
+      // add value to the cell
+      cell.innerHTML = array[j];
+    }
+  }
+  // Hide first column of the table (with database keys)
+  for (var i = 0; i <= keys.length; i++) {
+    table.rows[i].cells[0].style.display = 'none';
+  }
+  for (var i = 1; i <= keys.length; i++) {
+    table.rows[i].cells[2].id = 'colName';
+    table.rows[i].cells[4].id = 'colFxt';
+    table.rows[i].cells[7].id = 'colQty';
+  }
+
+}
+
+//------------------------------------------------------------------
+
 
 createTable_1();
 createTable_2();
 createTable_3();
+createTable_4();
 createTable_5();
 createTable_6();
 createTable_7();
+createTable_8();
+
+createInputs();
 
 
 // ============ Firebase Config ===============
@@ -720,6 +900,9 @@ ref.on('value', gotData_2, errData);
 ref = database.ref('lamps/test/out');
 ref.on('value', gotData_3, errData);
 
+ref = database.ref('lamps/test/store');
+ref.on('value', gotData_4, errData);
+
 ref = database.ref('lamps/real/store');
 ref.on('value', gotData_5, errData);
 
@@ -728,3 +911,6 @@ ref.on('value', gotData_6, errData);
 
 ref = database.ref('lamps/real/out');
 ref.on('value', gotData_7, errData);
+
+ref = database.ref('lamps/real/store');
+ref.on('value', gotData_8, errData);
